00783f963d65c67daad5019d6c7aa75d02e4637f
devbruto93 <douglas.modest.2013@gmail.com>
2026-05-11 14:45:17 -0300

    subindo o projeto pro github

---
+++ src/main/java/barbearia_api/BarbeariaApiApplication.java
@@ -0,0 +1,17 @@
+package barbearia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("TESTE.....TESTE......TESTE...");
+		
+		for(int i = 10; i>= 0; i --){
+			System.out.println("Iniciando em - " + i);
+		}
+	}
+
+}
