99deb784fe6cfd797d24218ee2653c2b05315e62
devbruto93 <douglas.modest.2013@gmail.com>
2026-05-20 08:11:00 -0300

    README

---
+++ README.mk
@@ -0,0 +1,19 @@
+# APRESENTAÇÃO
+
+Olá, este é um projeto pessoal que utilizarei para consolidar meus estudos.  
+Como ainda estou estudando, trabalharei neste projeto aos finais de semana, então os commits não serão tão frequentes quanto eu gostaria.
+
+# TECNOLOGIAS
+
+- Java
+- Spring Boot
+- JWT
+- PostgreSQL
+- Docker
+
+# FUTURAS FEATURES
+
+- JavaScript no front-end ou talvez eu “vibe code” o front
+- Integração de IA para otimizar os agendamentos, verificar clientes frequentes, calcular quanto tempo falta até o próximo corte, entre outras ideias
+
+Cada nova ideia que eu tiver será atualizada aqui.
