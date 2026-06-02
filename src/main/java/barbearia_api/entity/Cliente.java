e5f22b38cae48b0dccbdaee9a1bb9d7ac9820ec6
devbruto93 <douglas.modest.2013@gmail.com>
2026-06-02 16:10:30 -0300

    criação da classe cliente

---
+++ src/main/java/barbearia_api/entity/Cliente.java
@@ -0,0 +1,48 @@
+package barbearia_api.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String nome;
+
+  @Column(nullable = false, unique = true)
+  private String telefone;
+
+  public Cliente(){
+  }
+
+  public Cliente(Long id, String nome, String telefone){
+    this.id = id;
+    this.nome = nome;
+    this.telefone = telefone;
+  }
+
+  public Long getId(){
+    return id;
+  }
+  public void setId(Long id){
+    this.id = id;
+  }
+
+  public String getNome(){
+    return nome;
+  }
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public String getTelefone(){
+    return telefone;
+  }
+  public void setTelefone(String telefone){
+    this.telefone = telefone;
+  }
+
+}
