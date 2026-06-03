27576bc49da6e6ddc2814641b5e95134e00bdb55
devbruto93 <douglas.modest.2013@gmail.com>
2026-06-03 15:52:55 -0300

    criação da interface ClienteRepository e da classe ClienteService

---
+++ src/main/java/barbearia_api/service/ClienteService.java
@@ -0,0 +1,35 @@
+package barbearia_api.service;
+
+import barbearia_api.entity.Cliente;
+import barbearia_api.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+  private final ClienteRepository repository;
+
+  public ClienteService(ClienteRepository repository){
+    this.repository = repository;
+  
+  }
+
+  public Cliente salvar(Cliente cliente){
+    return repository.save(cliente);
+  }
+
+  public List<Cliente> listarTodos(){
+    return repository.findAll();
+  }
+
+  public Cliente buscarPorId(Long id){
+    return repository.findById(id)
+            .orElseThrow(()-> new RuntimeException("Cliente não encontrado!"));
+  }
+
+  public void deletar(Long id) {
+    repository.deleteById(id);
+  }
+
+}
