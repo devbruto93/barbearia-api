1aa2dd2bde71dd1faec4aba369a3d0f7d487a6c6
devbruto93 <douglas.modest.2013@gmail.com>
2026-06-22 10:50:02 -0300

    Atualização no cliente controller e DTO, e correção de bugs ate o momento

--- src/main/java/barbearia_api/controller/ClienteController.java
+++ src/main/java/barbearia_api/controller/ClienteController.java
@@ -1,8 +1,10 @@
 package barbearia_api.controller;
 
 import barbearia_api.entity.Cliente;
-import barbeariaapi.service.ClienteService;
+import barbearia_api.service.ClienteService;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+import barbearia_api.dto.ClienteDTO;
 
 import java.util.List;
 
@@ -17,17 +19,23 @@ public class ClienteController {
 	}
 
 	@PostMapping
+	public Cliente cadastrar(
+		@Valid @RequestBody ClienteDTO dto){
+
+		Cliente cliente = new Cliente();
+		cliente.setNome(dto.getNome());
+		cliente.setTelefone(dto.getTelefone());
+
+		return service.salvar(cliente);
+	}
+
+	@GetMapping
 	public List<Cliente> listarTodos(){
 		return service.listarTodos();
 	}
 
-	@GetMapping
-	pubic List<Cliente> listarTodos(){
-		return service.listarTodos();
-	}
-
 	@GetMapping("/{id}")
-	pubic Cliente buscarPorId(@PathVariable Long id){
+	public Cliente buscarPorId(@PathVariable Long id){
 		return service.buscarPorId(id);
 	}
 
--- src/main/java/barbearia_api/dto/ClienteDTO.java
+++ src/main/java/barbearia_api/dto/ClienteDTO.java
@@ -1,1 +1,44 @@
-ClienteDTO.java
+package barbearia_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class ClienteDTO{
+
+
+	@NotBlank(message = "Nome é obrigatório")
+	@Size(min = 3, max = 100,
+				message = "Nome de ter entre 3 e 100 caracteres")
+	private String nome;
+
+	@NotBlank(message = "Telefone é obrigatório")
+	@Pattern(
+			regexp = "^[0-9]{10,11}$",
+			message = "Telefone deve conter 10 ou 11 números")
+	private String telefone;
+
+	public ClienteDTO(){
+	}
+
+	public ClienteDTO(String nome, String telefone){
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getTelefone(){
+		return telefone;
+	}
+
+	public void setTelefone(String telefone){
+		this.telefone = telefone;
+	}
+}
